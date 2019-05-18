be47b7c6b255d65484be1e7f4f11f3ab0c51417d
Alexandru <alexandrusolot@gmail.com>
2019-05-18 23:18:40 +0100

    Added test cases

---
+++ tests/cdet_visdrone_eesp_lp_512.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $HOME/MThesis/repos/mine/light_obj_detection/SolotNet
+# train
+python train.py ctdet --exp_id visdrone_eesp_lp_512 --arch eesp_lp --dataset visdrone --input_res 512 --num_epochs 70 --lr_step 45,60 --gpus 0
+# test
+python evaluate.py ctdet --exp_id visdrone_eesp_lp_512 --arch eesp_lp --dataset visdrone --input_res 512 --resume
+# flip test
+python evaluate.py ctdet --exp_id visdrone_eesp_lp_512 --arch eesp_lp --dataset visdrone --input_res 512 --resume --flip_test
