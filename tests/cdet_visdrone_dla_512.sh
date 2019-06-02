56247575d9e077f8c894d6f423c96fcd2510c953
Alexandru <alexandrusolot@gmail.com>
2019-06-02 15:57:33 +0100

    updated test scripts

--- tests/cdet_visdrone_dla_512.sh
+++ tests/cdet_visdrone_dla_512.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $HOME/MThesis/repos/mine/light_obj_detection/SolotNet
 # train
-python train.py ctdet --exp_id visdrone_dla_512 --dataset visdrone --input_res 512 --num_epochs 70 --lr_step 45,60 --gpus 0
+python train.py ctdet --exp_id visdrone_dla_512 --batch_size 16 --dataset visdrone --input_res 512 --num_epochs 70 --lr_step 45,60 --gpus 0 --resume
 # test
 python evaluate.py ctdet --exp_id visdrone_dla_512 --dataset visdrone --input_res 512 --resume
 # flip test
