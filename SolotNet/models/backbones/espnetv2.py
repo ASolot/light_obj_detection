40baba5265d079d373b0366486fbe08fbd08b37e
Alexandru <alexandrusolot@gmail.com>
2019-06-04 19:00:35 +0100

    added improvements to espnetv2

--- SolotNet/models/backbones/espnetv2.py
+++ SolotNet/models/backbones/espnetv2.py
@@ -351,6 +351,8 @@ class EESPNet(nn.Module):
 
         global config_inp_reinf
         config_inp_reinf = 3
+
+        self.inplanes = 64
         self.input_reinforcement = True # True for the shortcut connection with input
 
         assert len(K) == len(r_lim), 'Length of branching factor array and receptive field array should be the same.'
@@ -378,6 +380,12 @@ class EESPNet(nn.Module):
         self.level5.append(CBR(config[4], config[4], 3, 1, groups=config[4]))
         self.level5.append(CBR(config[4], config[5], 1, 1, groups=K[4]))
 
+        self.deconv_layers = self._make_deconv_layer(
+            3,
+            [256, 128, 64],
+            [4, 4, 4],
+        )
+
         # TODO: support other heads
         self.heads = heads
         for head in self.heads:
@@ -404,9 +412,6 @@ class EESPNet(nn.Module):
                     fill_fc_weights(fc)
             self.__setattr__(head, fc)
 
-
-        # self.classifier = nn.Linear(config[5], classes)
-
         # init weights 
         self.init_params()
 
@@ -426,6 +431,64 @@ class EESPNet(nn.Module):
                 nn.init.normal_(m.weight, std=0.001)
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
+                    
+        # init of deconv layers
+        for name, m in self.deconv_layers.named_modules():
+            if isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _get_deconv_cfg(self, deconv_kernel, index):
+        if deconv_kernel == 4:
+            padding = 1
+            output_padding = 0
+        elif deconv_kernel == 3:
+            padding = 1
+            output_padding = 1
+        elif deconv_kernel == 2:
+            padding = 0
+            output_padding = 0
+
+        return deconv_kernel, padding, output_padding
+
+    def _make_deconv_layer(self, num_layers, num_filters, num_kernels):
+        assert num_layers == len(num_filters), \
+            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
+        assert num_layers == len(num_kernels), \
+            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
+
+        layers = []
+        for i in range(num_layers):
+            kernel, padding, output_padding = \
+                self._get_deconv_cfg(num_kernels[i], i)
+
+            planes = num_filters[i]
+            fc = DCN(self.inplanes, planes, 
+                    kernel_size=(3,3), stride=1,
+                    padding=1, dilation=1, deformable_groups=1)
+            # fc = nn.Conv2d(self.inplanes, planes,
+            #         kernel_size=3, stride=1, 
+            #         padding=1, dilation=1, bias=False)
+            # fill_fc_weights(fc)
+            up = nn.ConvTranspose2d(
+                    in_channels=planes,
+                    out_channels=planes,
+                    kernel_size=kernel,
+                    stride=2,
+                    padding=padding,
+                    output_padding=output_padding,
+                    bias=self.deconv_with_bias)
+            fill_up_weights(up)
+
+            layers.append(fc)
+            layers.append(nn.BatchNorm2d(planes, momentum=BN_MOMENTUM))
+            layers.append(nn.ReLU(inplace=True))
+            layers.append(up)
+            layers.append(nn.BatchNorm2d(planes, momentum=BN_MOMENTUM))
+            layers.append(nn.ReLU(inplace=True))
+            self.inplanes = planes
+
+        return nn.Sequential(*layers)
 
     def forward(self, input, p=0.2):
         '''
@@ -463,9 +526,12 @@ class EESPNet(nn.Module):
         # output_g = F.dropout(output_g, p=p, training=self.training)
         # output_1x1 = output_g.view(output_g.size(0), -1)
 
+        # deconv layers here 
+        x = self.deconv_layers(out_l5)
+
         ret = {} 
         for head in self.heads:
-            ret[head] = self.__getattr__(head)(out_l5)
+            ret[head] = self.__getattr__(head)(x)
         return [ret]
 
 def get_espv2_net(num_layers, heads, head_conv=256):
--- bash_scripts/module_loader.sh
+++ bash_scripts/module_loader.sh
@@ -6,15 +6,8 @@
 
 # conda environment
 module load miniconda3-4.5.4-gcc-5.4.0-hivczbz
-
-# # CUDA 10.0 & cudnn
-# module load cuda/10.0 intel/mkl/2017.4 cudnn/7.5_cuda-10.0
-
-module unload cuda/8.0
-# for unknown reasons, tensorboard won't work 
+module unload cuda/8.0 
 module load cuda/10.0
-
-# CUDA 9.0 & cudnn 
 module load cuda/9.0 intel/mkl/2017.4 
 module load cudnn/7.3_cuda-9.0
 
