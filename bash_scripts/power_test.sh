88559bdfb307a684741322f76d8e145bef4d93b7
Alexandru <alexandrusolot@gmail.com>
2019-06-07 20:46:48 +0100

    Script for inference testing

--- bash_scripts/power_test.sh
+++ bash_scripts/power_test.sh
@@ -3,15 +3,15 @@ cd $HOME/MThesis/repos/mine/light_obj_detection/SolotNet
 
 echo "Start DLA_C_1024\n"
 echo "Infer: 1024\n"
-python evaluate.py ctdet --exp_id visdrone_dla_c_1024 --dataset visdrone --input_res 1024 --resume
+python3 evaluate.py ctdet --exp_id visdrone_dla_c_1024 --dataset visdrone --input_res 1024 --resume
 sleep 10
 
 echo "Infer: 512\n"
-python evaluate.py ctdet --exp_id visdrone_dla_c_1024 --dataset visdrone --input_res 512 --resume
+python3 evaluate.py ctdet --exp_id visdrone_dla_c_1024 --dataset visdrone --input_res 512 --resume
 sleep 10
 
 echo "Infer: orig\n"
-python evaluate.py ctdet --exp_id visdrone_dla_c_1024 --dataset visdrone --keep_res--resume
+python3 evaluate.py ctdet --exp_id visdrone_dla_c_1024 --dataset visdrone --keep_res--resume
 sleep 10
 
 
@@ -19,15 +19,15 @@ sleep 10
 sleep 10
 echo "Start DLA_C_512\n"
 echo "Infer: 1024\n"
-python evaluate.py ctdet --exp_id visdrone_dla_c_512 --dataset visdrone --input_res 1024 --resume
+python3 evaluate.py ctdet --exp_id visdrone_dla_c_512 --dataset visdrone --input_res 1024 --resume
 sleep 10
 
 echo "Infer: 512\n"
-python evaluate.py ctdet --exp_id visdrone_dla_c_512 --dataset visdrone --input_res 512 --resume
+python3 evaluate.py ctdet --exp_id visdrone_dla_c_512 --dataset visdrone --input_res 512 --resume
 sleep 10
 
 echo "Infer: orig\n"
-python evaluate.py ctdet --exp_id visdrone_dla_c_512 --dataset visdrone --keep_res --resume
+python3 evaluate.py ctdet --exp_id visdrone_dla_c_512 --dataset visdrone --keep_res --resume
 sleep 10
 
 
@@ -35,15 +35,15 @@ sleep 10
 sleep 10
 echo "START ESPNET_C_1024\n"
 echo "Infer: 1024\n"
-python evaluate.py ctdet --exp_id visdrone_esp_c_1024 --arch espnetv2 --dataset visdrone --input_res 1024 --resume
+python3 evaluate.py ctdet --exp_id visdrone_esp_c_1024 --arch espnetv2 --dataset visdrone --input_res 1024 --resume
 sleep 10
 
 echo "Infer: 512\n"
-python evaluate.py ctdet --exp_id visdrone_esp_c_1024 --arch espnetv2 --dataset visdrone --input_res 512 --resume
+python3 evaluate.py ctdet --exp_id visdrone_esp_c_1024 --arch espnetv2 --dataset visdrone --input_res 512 --resume
 sleep 10
 
 echo "Infer: orig\n"
-python evaluate.py ctdet --exp_id visdrone_esp_c_1024 --arch espnetv2 --dataset visdrone --keep_res --resume
+python3 evaluate.py ctdet --exp_id visdrone_esp_c_1024 --arch espnetv2 --dataset visdrone --keep_res --resume
 sleep 10
 
 
@@ -52,15 +52,15 @@ sleep 10
 sleep 10
 echo "START ESPNET_C_512\n"
 echo "Infer: 1024\n"
-python evaluate.py ctdet --exp_id visdrone_esp_c_512 --arch espnetv2 --dataset visdrone --input_res 1204 --resume
+python3 evaluate.py ctdet --exp_id visdrone_esp_c_512 --arch espnetv2 --dataset visdrone --input_res 1204 --resume
 sleep 10
 
 echo "Infer: 512\n"
-python evaluate.py ctdet --exp_id visdrone_esp_c_512 --arch espnetv2 --dataset visdrone --input_res 512 --resume
+python3 evaluate.py ctdet --exp_id visdrone_esp_c_512 --arch espnetv2 --dataset visdrone --input_res 512 --resume
 sleep 10
 
 echo "Infer: orig\n"
-python evaluate.py ctdet --exp_id visdrone_esp_c_512 --arch espnetv2 --dataset visdrone --keep_res --resume
+python3 evaluate.py ctdet --exp_id visdrone_esp_c_512 --arch espnetv2 --dataset visdrone --keep_res --resume
 sleep 10
 
 
@@ -70,15 +70,15 @@ sleep 10
 sleep 10
 echo "START_RESNET_1024\n"
 echo "Infer: 1024\n"
-python evaluate.py ctdet --exp_id visdrone_res_1024 --arch resdcn_18 --dataset visdrone --input_res 1024 --resume
+python3 evaluate.py ctdet --exp_id visdrone_res_1024 --arch resdcn_18 --dataset visdrone --input_res 1024 --resume
 sleep 10
 
 echo "Infer: 512\n"
-python evaluate.py ctdet --exp_id visdrone_res_1024 --arch resdcn_18 --dataset visdrone --input_res 512 --resume
+python3 evaluate.py ctdet --exp_id visdrone_res_1024 --arch resdcn_18 --dataset visdrone --input_res 512 --resume
 sleep 10
 
 echo "Infer: orig\n"
-python evaluate.py ctdet --exp_id visdrone_res_1024 --arch resdcn_18 --dataset visdrone --keep_res --resume
+python3 evaluate.py ctdet --exp_id visdrone_res_1024 --arch resdcn_18 --dataset visdrone --keep_res --resume
 sleep 10
 
 
